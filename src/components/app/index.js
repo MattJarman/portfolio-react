64202bf15be2fde3ab931189ba3f9c26274c212e
matt <mattjarman22@outlook.com>
2020-07-12 12:25:35 +0100

    Fix navbar on safari iphone

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -78,7 +78,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col h-screen">
+      <div className="h-screen">
         <Navbar />
         <main className="flex-grow">
           <AnimatedSwitch {...slide}>
